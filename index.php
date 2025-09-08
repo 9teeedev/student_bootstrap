5c1694bc40fa14ede128a482958bacc12634ad61
9teeedev <teezeezax2@gmail.com>
2025-09-08 10:15:39 +0700

    Add feature search and page number

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php 
     require "connect.php";
     if($conn){
-
-    function convert_date($date){
-        $date = explode("-", $date);
-        $month = array("01"=>"มกราคม","02"=>"กุมภาพันธ์","03"=>"มีนาคม","04"=>"เมษายน","05"=>"พฤษภาคม","06"=>"มิถุนายน","07"=>"กรกฎาคม","08"=>"สิงหาคม","09"=>"กันยายน","10"=>"ตุลาคม","11"=>"พฤศจิกายน","12"=>"ธันวาคม");
-        return "วันที่ ".$date[2]." ".$month[$date[1]]." ".($date[0]+543);
-    }
+        $page = isset($_GET['page']) ? trim($_GET['page']) : '';
+        if($page == '') $page = 1;
+        function convert_date($date){
+            $date = explode("-", $date);
+            $month = array("01"=>"มกราคม","02"=>"กุมภาพันธ์","03"=>"มีนาคม","04"=>"เมษายน","05"=>"พฤษภาคม","06"=>"มิถุนายน","07"=>"กรกฎาคม","08"=>"สิงหาคม","09"=>"กันยายน","10"=>"ตุลาคม","11"=>"พฤศจิกายน","12"=>"ธันวาคม");
+            return "วันที่ ".$date[2]." ".$month[$date[1]]." ".($date[0]+543);
+        }
 ?>
 
 <!DOCTYPE html>
@@ -30,6 +31,16 @@
 <body class="bg-light">
     <div class="container my-5">
         <h1 class="text-center text-primary mb-4 display-4">ระบบจัดการข้อมูลนักศึกษา</h1>
+        <form action="" method="get" class="d-flex justify-content-center mb-4">
+            <div class="input-group w-50">
+                <input type="text" name="textsearch" id="textsearch" class="form-control" placeholder="กรุณาป้อนคำค้นหา">
+                <button type="submit" class="btn btn-primary">ค้นหา</button>
+            </div>
+        </form>
+        <div class="text-center mt-4">
+            <a href="index.php" class="btn btn-primary me-2">แสดงข้อมูลทั้งหมด</a>
+            <a href="formadd.php" class="btn btn-success">เพิ่มข้อมูล</a>
+        </div>
         <div class="table-responsive">
             <table class="table table-striped table-hover table-bordered">
                 <thead class="table-dark">
@@ -44,10 +55,25 @@
                 </thead>
                 <tbody>
                     <?php
-                        $query = "SELECT * FROM student;";
+                        $textsearch = isset($_GET['textsearch']) ? trim($_GET['textsearch']) : '';
+                        $query = "SELECT * FROM student";
+                        if($textsearch != ""){
+                            $query = $query." WHERE stdid LIKE '%$textsearch%' OR stdname LIKE '%$textsearch%'";
+                        }  
                         $result = mysqli_query($conn, $query);
-                        $i=1;
 
+                        $recordall = mysqli_num_rows($result);
+                        $perpage = 10;
+
+                        $numpage = $recordall/$perpage;
+                        if($recordall % $perpage > 0) $numpage++;
+                        echo "<center class='mt-4'>ข้อมูลทั้งหมดมี ".$recordall." รายการ มี ".intval($numpage)." หน้า กำลังแสดงข้อมูลหน้าที่ ".$page."</center>";
+                        
+                        $start = $perpage*$page-$perpage;
+                        $queryview = $query." LIMIT $start, $perpage";  
+                        $result = mysqli_query($conn, $queryview);
+                        
+                        $i=1+$start;
                         while ($data = mysqli_fetch_row($result)) {
                     ?>
                     <tr>
@@ -61,11 +87,12 @@
                     <?php $i++; } ?>
                 </tbody>
             </table>
+            <div style="text-align: center;">
+                เลือกหน้า <?php for ($i=1; $i<=$numpage; $i++) echo "<a href='index.php?page=$i'>$i</a> |&nbsp;"?>
+                
+            </div>
         </div>
-        <div class="text-center mt-4">
-            <a href="index.php" class="btn btn-primary me-2">แสดงข้อมูลทั้งหมด</a>
-            <a href="formadd.php" class="btn btn-success">เพิ่มข้อมูล</a>
-        </div>
+        
     </div>
 </body>
 </html>
